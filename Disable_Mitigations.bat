1c018c6ae4241d47333fd971a337aeca2880046a
xnake1 <108781793+xnake1@users.noreply.github.com>
2022-07-16 05:17:47 -0400

    Add files via upload

---
+++ Disable_Mitigations.bat
@@ -0,0 +1,68 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+
+
+@echo off
+
+color 17
+echo Desactivando mitigaciones [...]
+
+timeout 1 > nul
+:: Spectre ^&^ Meltdown (Credit: CatGamerOP)
+reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "1" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet001\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet002\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "1" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet002\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet002\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\ControlSet002\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettings" /t REG_DWORD /d "1" /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverride" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "FeatureSettingsOverrideMask" /t REG_DWORD /d "3" /f >nul 2>&1
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "EnableCfg" /t REG_DWORD /d "0" /f >nul 2>&1
+timeout 1 > nul
+
+:: NTFS Mitigations (Credit: UnlovedCookie)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v "ProtectionMode" /t REG_DWORD /d "0" /f >nul 2>&1
+timeout 1 > nul
+
+:: Address Space Layout Randomization (ASLR)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v "MoveImages" /t REG_DWORD /d "0" /f >nul 2>&1
+timeout 1 > nul
+
+:: Structured Exception Handling Overwrite Protection (SEHOP)
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Kernel" /v "DisableExceptionChainValidation" /t REG_DWORD /d "1" /f >nul 2>&1
+timeout 1 > nul
+
+:: Data Excecution Prevention (DEP)
+bcdedit /set nx AlwaysOff >nul 2>&1
+timeout 1 > nul
+cls
+
+echo Script Finished!
+timeout 2 > nul
+cls
+
+echo presiona cualquier tecla para salir [...]
+pause > nul
+exit
+
+:: Microsoft's Powershell Command
+Powershell -Command "Set-ProcessMitigation -System -Disable DEP, EmulateAtlThunks, SEHOP, ForceRelocateImages, RequireInfo, BottomUp, HighEntropy, StrictHandle, DisableWin32kSystemCalls, AuditSystemCall, DisableExtensionPoints, BlockDynamicCode, AllowThreadsToOptOut, AuditDynamicCode, CFG, SuppressExports, StrictCFG, MicrosoftSignedOnly, AllowStoreSignedBinaries, AuditMicrosoftSigned, AuditStoreSigned, EnforceModuleDependencySigning, DisableNonSystemFonts, AuditFont, BlockRemoteImageLoads, BlockLowLabelImageLoads, PreferSystem32, AuditRemoteImageLoads, AuditLowLabelImageLoads, AuditPreferSystem32, EnableExportAddressFilter, AuditEnableExportAddressFilter, EnableExportAddressFilterPlus, AuditEnableExportAddressFilterPlus, EnableImportAddressFilter, AuditEnableImportAddressFilter, EnableRopStackPivot, AuditEnableRopStackPivot, EnableRopCallerCheck, AuditEnableRopCallerCheck, EnableRopSimExec, AuditEnableRopSimExec, SEHOP, AuditSEHOP, SEHOPTelemetry, TerminateOnError, DisallowChildProcessCreation, AuditChildProcess" >nul 2>&1
+cls
+echo Script Finalizado!
+timeout 2 > nul
+echo presiona cualquier tecla para salir...
+pause > nul
+exit
---
+++ Tweak_Device_Manager.bat
@@ -0,0 +1,73 @@
+@echo off&color 17
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+
+
+
+
+@echo off
+echo .
+cls
+powershell Invoke-WebRequest "https://cdn.discordapp.com/attachments/919533685785632798/944524976323715093/DevManView.exe" -OutFile "C:\Windows\DevManView.exe"
+echo Aplicando Device Manager cfgs[...]
+timeout /T 1 /NOBREAK > nul
+devmanview /disable "Microsoft System Management BIOS Driver"
+devmanview /disable "Programmable Interrupt Controller"
+devmanview /disable "High Precision Event Timer"
+devmanview /disable "PCI Encryption/Decryption Controller"
+devmanview /disable "AMD PSP"
+devmanview /disable "Intel SMBus"
+devmanview /disable "System Speaker"
+devmanview /disable "System Timer"
+devmanview /disable "WAN Miniport (IKEv2)"
+devmanview /disable "WAN Miniport (IP)"
+devmanview /disable "WAN Miniport (IPv6)"
+devmanview /disable "WAN Miniport (L2TP)"
+devmanview /disable "WAN Miniport (Network Monitor)"
+devmanview /disable "WAN Miniport (PPPOE)"
+devmanview /disable "WAN Miniport (PPTP)"
+devmanview /disable "WAN Miniport (SSTP)"
+devmanview /disable "UMBus Root Bus Enumerator"
+devmanview /disable "Intel Management Engine"
+devmanview /disable "PCI Memory Controller"
+devmanview /disable "PCI standard RAM Controller"
+devmanview /disable "Composite Bus Enumerator"
+devmanview /disable "Microsoft Kernel Debug Network Adapter"
+devmanview /disable "SM Bus Controller"
+devmanview /disable "NDIS Virtual Network Adapter Enumerator"
+devmanview /disable "Numeric Data Processor"
+devmanview /disable "Microsoft RRAS Root Enumerator"
+cls
+goto power
+
+:power
+echo X Device [...]
+timeout /T 1 /NOBREAK > nul
+for /F "tokens=*" %%A in ('powercfg -devicequery wake_armed') do powercfg -devicedisablewake "%%A"
+cls
+goto op
+
+:op
+echo Eliminando ahorro de energia de USBs[...]
+timeout /T 1 /NOBREAK > nul
+for /f "tokens=*" %%i in ('wmic PATH Win32_PnPEntity GET DeviceID ^| findstr "USB\VID_"') do (
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "EnhancedPowerManagementEnabled" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "AllowIdleIrpInD3" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "EnableSelectiveSuspend" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "DeviceSelectiveSuspended" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "SelectiveSuspendEnabled" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "SelectiveSuspendOn" /t REG_DWORD /d "0" /f > nul
+ reg add "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Enum\%%i\Device Parameters" /v "D3ColdSupported" /t REG_DWORD /d "0" /f > nul
+)
+powershell -Command "$devices = Get-WmiObject Win32_PnPEntity; $powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi; foreach ($p in $powerMgmt){$IN = $p.InstanceName.ToUpper(); foreach ($h in $devices){$PNPDI = $h.PNPDeviceID; if ($IN -like \"*$PNPDI*\"){$p.enable = $False; $p.psbase.put()}}}" >nul 2>nul
+cls
+exit
