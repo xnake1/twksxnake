1d0123962cc67f072cd75c026e1c6bd72004d8ca
xnake1 <108781793+xnake1@users.noreply.github.com>
2022-07-16 03:29:06 -0400

    Update DiscordDEB.bat

--- DiscordDEB.bat
+++ DiscordDEB.bat
@@ -1,3 +1,19 @@
+@echo off&color 17
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+cd /d "%~dp0"
+The current running path of echo is% CD%
+Echo has obtained administrator rights
+
+
+
+
+
 @echo off
 TITLE Optimizador de Discord
 
